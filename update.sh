583dbc3304cd66048829d8eb9d72a327cf2472c6
Tony <aector4@gmail.com>
2026-02-19 13:46:56 +0000

    Add update system

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "Checking for ThinkPad OS updates..."
+
+cd "$HOME/thinkpad-os" || exit 1
+
+LOCAL_VERSION=$(cat VERSION)
+LATEST_VERSION=$(curl -s https://raw.githubusercontent.com/Aector84/thinkpad-os/main/VERSION)
+
+if [ "$LOCAL_VERSION" = "$LATEST_VERSION" ]; then
+    echo "Already up to date. Version $LOCAL_VERSION"
+    exit 0
+fi
+
+echo "Updating from $LOCAL_VERSION to $LATEST_VERSION..."
+
+git pull
+./install.sh
+
+echo "$LATEST_VERSION" > VERSION
+
+echo "Update complete."
