de7c6c9e4b485e5cc6031e3bc9bade1afbf78730
Tony <aector4@gmail.com>
2026-02-19 13:51:01 +0000

    Fix update version comparison logic

--- update.sh
+++ update.sh
@@ -4,8 +4,13 @@ echo "Checking for ThinkPad OS updates..."
 
 cd "$HOME/thinkpad-os" || exit 1
 
-LOCAL_VERSION=$(cat VERSION)
-LATEST_VERSION=$(curl -s https://raw.githubusercontent.com/Aector84/thinkpad-os/main/VERSION)
+LOCAL_VERSION=$(cat VERSION | tr -d '[:space:]')
+LATEST_VERSION=$(curl -s https://raw.githubusercontent.com/Aector84/thinkpad-os/main/VERSION | tr -d '[:space:]')
+
+if [ -z "$LATEST_VERSION" ]; then
+    echo "Could not retrieve latest version."
+    exit 1
+fi
 
 if [ "$LOCAL_VERSION" = "$LATEST_VERSION" ]; then
     echo "Already up to date. Version $LOCAL_VERSION"
@@ -19,4 +24,4 @@ git pull
 
 echo "$LATEST_VERSION" > VERSION
 
-echo "Update complete."
+echo "Update complete. Now running version $LATEST_VERSION"
